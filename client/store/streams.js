641af5e032309b7bb934ed3e10879c9072cea28e
blydiet <lydietoussaint123@gmail.com>
2019-08-10 15:18:19 -0400

    (feature-Added-CRUD-To-Store)- the user should be able to update and delete now

--- client/store/streams.js
+++ client/store/streams.js
@@ -1,27 +1,98 @@
 import axios from 'axios'
 
-const GET_STREAM = 'NEW_STREAM'
-
+const GET_STREAMS = 'NEW_STREAMS'
+const GET_NEWSTREAM = 'GET_NEWSTREAM'
+const GET_STREAM = 'GET_STREAM'
+const REMOVE_STREAM = 'REMOVE_STREAM'
+const EDIT_STREAM = 'EDIT_STREAM'
 const streams = {
-  stream: []
+  allStreams: [],
+  stream: {}
 }
 
 const getStream = stream => ({type: GET_STREAM, stream})
-
+const getNewStream = stream => ({type: GET_NEWSTREAM, stream})
+const getStreams = stream => ({type: GET_STREAMS, stream})
+const removeStream = streamId => ({type: REMOVE_STREAM, streamId})
+const updateStream = (streamId, updatedStream) => ({
+  type: EDIT_STREAM,
+  streamId,
+  updateStream
+})
 export const createStream = (title, description) => async dispatch => {
   try {
-    const res = await axios.post('/api/streams', {title, description})
-    console.log(res)
-    dispatch(getStream(res.data))
+    const {data} = await axios.post('/api/streams', {title, description})
+    dispatch(getNewStream(data))
   } catch (newStreamError) {
     console.log(newStreamError)
+  }
+}
+export const fetchStream = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/streams/:id')
+    dispatch(getStream(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const fetchStreams = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/streams')
+    dispatch(getStreams(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const removeSteam = streamId => async dispatch => {
+  try {
+    const removedSteam = await axios.delete(`/api/streams/${streamId}`)
+    dispatch(removeSteam(streamId))
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const streamUpdate = (
+  streamId,
+  title,
+  description
+) => async dispatch => {
+  try {
+    const {data} = await axios.put(`/api/streams/${streamId}`, {
+      title,
+      description
+    })
+    dispatch(updateStream(data))
+  } catch (error) {
+    console.log(error)
   }
 }
 
 export default function(state = streams, action) {
   switch (action.type) {
+    case GET_NEWSTREAM:
+      return action.stream
     case GET_STREAM:
       return action.stream
+    case GET_STREAMS:
+      return {...state, allStreams: action.stream}
+    case REMOVE_STREAM:
+      return {
+        ...state,
+        allStreams: state.allStreams.filter(
+          streams => stream.id !== action.streamId
+        )
+      }
+    case EDIT_STREAM:
+      return {
+        ...state,
+        allStreams: state.allStreams.map(
+          streams =>
+            streams.id === action.streamId
+              ? {...streams, stream: action.updateStream}
+              : streams
+        )
+      }
     default:
       return state
   }
--- server/api/streams.js
+++ server/api/streams.js
@@ -2,6 +2,26 @@ const router = require('express').Router()
 const {Streams} = require('../db/models')
 module.exports = router
 
+router.get('/', async (req, res, next) => {
+  try {
+    const allStreams = await Streams.findAll()
+    res.json(allStreams).status(200)
+  } catch (error) {
+    console.log(error)
+  }
+})
+router.get('/:id', async (req, res, next) => {
+  try {
+    const singleStream = await Streams.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(singleStream).status(201)
+  } catch (error) {
+    console.log(error)
+  }
+})
 router.post('/', async (req, res, next) => {
   try {
     const newStream = await Streams.create({
@@ -11,5 +31,31 @@ router.post('/', async (req, res, next) => {
     res.json(newStream).status(201)
   } catch (error) {
     next(error)
+  }
+})
+router.delete('/delete/:id', async (req, res, next) => {
+  try {
+    const streamId = await Streams.findByPk(req.params.id)
+    if (!streamId) return res.sendStatus(404)
+    const remove = await Streams.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.sendStatus(204)
+  } catch (error) {
+    console.log(error)
+  }
+})
+router.put('/update/:id', async (req, res, next) => {
+  try {
+    const streamId = await Streams.findByPk(req.params.id)
+    if (!streamId) return res.sendStatus(404)
+    const update = await streamId.update({
+      title: req.body.title,
+      description: req.body.description
+    })
+  } catch (error) {
+    console.log(error)
   }
 })
