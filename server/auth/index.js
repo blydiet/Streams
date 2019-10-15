74df0b6fe2cd75bf01c6d8cf90d753e084374160
blydiet <lydietoussaint123@gmail.com>
2019-10-15 18:29:45 -0400

    (feature-Adding-Security-With-JWT)- I want to learn how to add security that used JWT

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const User = require('../db/models/user')
+const jwt = require('jwt-simple')
 module.exports = router
+
+const tokenForUser = user => {
+  const timeStamp = new Date().getTime()
+  return jwt.encode({sub: user.id, iat: timeStamp}, process.env.secret)
+}
 
 router.post('/login', async (req, res, next) => {
   try {
@@ -12,7 +18,10 @@ router.post('/login', async (req, res, next) => {
       console.log('Incorrect password for user:', req.body.email)
       res.status(401).send('Wrong username and/or password')
     } else {
-      req.login(user, err => (err ? next(err) : res.json(user)))
+      req.login(
+        user,
+        err => (err ? next(err) : res.json({token: tokenForUser(user)}))
+      )
     }
   } catch (err) {
     next(err)
@@ -22,7 +31,10 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
-    req.login(user, err => (err ? next(err) : res.json(user)))
+    req.login(
+      user,
+      err => (err ? next(err) : res.json({token: tokenForUser(user)}))
+    )
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists')
